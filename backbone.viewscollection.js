49a6bec1d5132fcf4ab7e3ae0e5cb509c00fdf92
gianbot <gb@blowjohn.it>
2013-02-22 15:00:57 +0100

    All test clear

--- backbone.viewscollection.js
+++ backbone.viewscollection.js
@@ -3,37 +3,73 @@
 // Extend standard collection class for our sets of views, ordered
 // or unordered. If a `comparator` is specified, the Collection will maintain
 // its views in sort order, as they're added and removed.
-!function(){
+! function() {
 
-  var Collection = Backbone.ViewsCollection = function(views, options) {
-    options || (options = {});
-    if (options.view) this.view = options.view;
-    if (options.comparator !== void 0) this.comparator = options.comparator;
-    this.model = this.view;
-    this.models = this.views = [];
-    this._reset();
-    this.initialize.apply(this, arguments);
-    if (views) this.reset(views, _.extend({silent: true}, options));
-  };
+    var Collection = Backbone.ViewsCollection = function(views, options) {
+        options || ( options = {});
+        if (options.view)
+            this.view = options.view;
+        if (options.comparator !==
+            void 0)
+            this.comparator = options.comparator;
+        this.model = this.view;
+        this.models = this.views = [];
+        this._reset();
+        this.initialize.apply(this, arguments);
+        if (views)
+            this.reset(views, _.extend({
+                silent : true
+            }, options));
+    };
 
-  _.extend(Collection.prototype, Backbone.Collection.prototype, {
+    _.extend(Collection.prototype, Backbone.Collection.prototype, {
 
-    view: Backbone.View,
+        view : Backbone.View,
 
-    sync: function() {
-      // View not need to sync...
-      return this;
-    },
+        sync : function() {
+            // View not need to sync...
+            return this;
+        },
+        
+        // Create a new instance of a model in this collection. Add the model to the
+        // collection immediately, unless `wait: true` is passed, in which case we
+        // wait for the server to agree.
+        create: function(model, options) {
+          options = options ? _.clone(options) : {};
+          // TODO: default view creation...
+          if(!model){
+              model = new this.view();
+          }
+          if (!(model = this._prepareModel(model, options))) return false;
+          this.add(model, options);
+          return model;
+        },
 
-    _prepareModel: function(attrs, options) {
-      if (attrs instanceof Backbone.View) {
-        if (!attrs.collection) attrs.collection = this;
-        return attrs;
-      }
-      // Only Backbone.View allowed!
-      return false;
-    }
+        _prepareModel : function(attrs, options) {
+            if ( attrs instanceof Backbone.View) {
+                if (!attrs.viewCollection)
+                    attrs.viewCollection = this;
+                return attrs;
+            }
+            // Only Backbone.View allowed!
+            return false;
+        },
 
-  });
+        // Internal method to remove a model's ties to a collection.
+        _removeReference : function(model) {
+            if (this === model.viewCollection)
+                delete model.viewCollection;
+            model.off('all', this._onModelEvent, this);
+        },
+    });
+    
+    var unusedMethods = ['toJSON', 'fetch'];
+
+    // Unused method
+    _.each(unusedMethods, function(meth){
+        delete(Collection.prototype['meth']);
+    });
+    // Copy extend form Backbone.Collection
+    Collection.extend = Backbone.Collection.extend;
 
 }();
--- tests/specs/base.js
+++ tests/specs/base.js
@@ -1,32 +1,46 @@
-describe("First suite - Initializing:", function(){
+describe("First suite - Initializing:", function() {
 
     var A;
-
-    it('Can be extendible', function(){
-      A = Backbone.ViewsCollection.extend();
-      expect(A).toBeDefined();
-    });
-
     var baseViewsCollection = new Backbone.ViewsCollection();
-    it('Have a views array', function(){
-      // Test a empty array
-      expect(baseViewsCollection.views.length).toBe(0);
-    });
-    it("Can be resetted", function(){
-      baseViewsCollection.reset();
-      expect(baseViewsCollection.views.length).toBe(0);
-    });
-    baseViewsCollection.reset();
-    it('Can add a view', function(){
-      expect(baseViewsCollection.views.length).toBe(0);
-      baseViewsCollection.add(new Backbone.View());
-      expect(baseViewsCollection.views.length).toBe(1);
+    
+    baseViewsCollection.on('all', function(){
+        console.log('event', arguments);
     })
-    baseViewsCollection.reset();
-    it("Can't add a model", function(){
-      baseViewsCollection.add(new Backbone.Model());
-      expect(baseViewsCollection.views.length).not.toBe(1);
-      console.log(baseViewsCollection.views);
+    
+    it('Can be extendible', function() {
+        A = Backbone.ViewsCollection.extend();
+        expect(A).toBeDefined();
+    });
+    it("Don't have a toJSON method", function() {
+        expect(A.toJSON).toBeUndefined();
     })
-
-});
+    it('Have a views array', function() {
+        // Test a empty array
+        expect(baseViewsCollection.views.length).toBe(0);
+    });
+    it("Can be resetted", function() {
+        baseViewsCollection.add(new Backbone.View());
+        baseViewsCollection.reset();
+        expect(baseViewsCollection.views.length).toBe(0);
+    });
+    it('Can add a view', function() {
+        baseViewsCollection.reset();
+        expect(baseViewsCollection.views.length).toBe(0);
+        baseViewsCollection.add(new Backbone.View());
+        expect(baseViewsCollection.views.length).toBe(1);
+    })
+    it("Can't add a model", function() {
+        
+        baseViewsCollection.reset();
+        expect(baseViewsCollection.views.length).toBe(0);
+        baseViewsCollection.add(new Backbone.Model());
+        expect(baseViewsCollection.views.length).not.toBe(1);
+    })
+    
+    it("Create a default view", function(){
+        baseViewsCollection.reset();
+        var view = baseViewsCollection.create();
+        expect(baseViewsCollection.views.length).toBe(1);
+        expect(view).toBeDefined();
+    });
+}); 
